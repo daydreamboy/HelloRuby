79ce873dcd04164246194566d1b270b4e9618b6d
daydreamboy <wesley4chen@gmail.com>
2019-09-07 14:20:32 +0800

    ruby_basic: add code

--- ruby_task/07_git-batch.rb
+++ ruby_task/07_git-batch.rb
@@ -16,37 +16,37 @@ class GitBatch
   attr_accessor :debugging
 
   def initialize
-    self.configuration = {
-        'git_repo_list' => [
-            'MPDataSDKExtention'
-        ]
-    }
-    self.global = OptionParser.new do |opts|
-      opts.banner = "Usage: #{__FILE__} <git subcommand> [-options] argument"
-      opts.separator  ""
-      opts.separator  "git批量工具"
-      opts.separator  ""
-      opts.separator  "示例：ruby /path/to/07_git-batch.rb log -d 10"
-      opts.separator  ""
-      opts.separator  "查看帮助：ruby /path/to/07_git-batch.rb --help"
-    end
+    self.global = OptionParser.new do |parser|
+      parser.banner = "Usage: #{__FILE__} <git subcommand> [-options] argument"
+      parser.separator  ""
+      parser.separator  "git批量工具"
+      parser.separator  ""
+      parser.separator  "示例：ruby /path/to/07_git-batch.rb log -d 10"
+      parser.separator  ""
+      parser.separator  "查看帮助：ruby /path/to/07_git-batch.rb --help"
 
-    # Boolean switch.
-    self.global.on("-d", "--[no-]debug", "Run with debug info") do |enabled|
-      self.debugging = enabled
+      # Boolean switch.
+      parser.on("-d", "--[no-]debug", "Run with debug info") do |enabled|
+        self.debugging = enabled
+      end
+
+      parser.on("-c", "--configuration [path/to/<config>.json]", String, "The configuration file path") do |file_path|
+        $CONFIG_FILE_PATH = file_path
+      end
     end
 
     self.subcommands = {
         # Note: configure more subcommand here...
         'log' => {
-            :optParser => OptionParser.new do |opts|
-              opts.banner = "Usage: #{__FILE__} log [-options] argument"
+            :optParser => OptionParser.new do |parser|
+              parser.banner = "Usage: #{__FILE__} log [-options] argument"
 
-              opts.on("-d", "--days N", Integer, "The number of recent days") do |days|
+              parser.on("-d", "--days N", Integer, "The number of recent days") do |days|
                 self.subcommands['log'][:options][:days] = days
               end
             end,
-            :action => Proc.new do
+            :action => Proc.new do |subcommandline|
+
               search_current_git_repos do |entry|
                 gitDirPath = File.join('./', entry, '.git')
                 if File.directory? File.join('./', entry) and File.directory? gitDirPath
@@ -81,8 +81,33 @@ class GitBatch
               end
             end,
             :options => {}
-        }
+        },
+        'pull' => {
+            :optParser => nil,
+            :action => Proc.new do |subcommandline|
+              run_git_command subcommandline
+            end
+        },
     }
+  end
+
+  def run_git_command(subcommandline)
+    search_current_git_repos do |entry|
+      if File.directory? File.join('./', entry) and File.directory? File.join('./', entry, '.git')
+        cmd = "cd #{entry} && git #{subcommandline}"
+        # dump_object(cmd)
+        puts "\033[32m[#{entry}]\033[0m"
+
+        if self.debugging then
+          puts "[Debug] #{cmd}"
+        else
+          content = `#{cmd}`
+          puts content
+        end
+
+        puts
+      end
+    end
   end
 
   def search_current_git_repos
@@ -103,36 +128,32 @@ class GitBatch
       return
     end
 
+    # Note: parse first level command
+    self.global.order!
+
     config_file_path = File.join '.', $CONFIG_FILE_PATH
     if File.exist? config_file_path
       self.configuration = JSON.parse File.read config_file_path
+      if self.debugging
+        puts "[Debug] #{self.configuration}"
+        puts
+      end
     end
 
-    self.global.order!
     subcommandline = ARGV.join(' ')
-    subcommands = ARGV.shift
+    subcommand = ARGV.shift
 
-    if self.subcommands[subcommands].nil?
-      search_current_git_repos do |entry|
-        if File.directory? File.join('./', entry) and File.directory? File.join('./', entry, '.git')
-          cmd = "cd #{entry} && git #{subcommandline}"
-          # dump_object(cmd)
-          puts "\033[32m[#{entry}]\033[0m"
-
-          if self.debugging then
-            puts "[Debug] #{cmd}"
-          else
-            content = `#{cmd}`
-            puts content
-          end
-
-          puts
-        end
-      end
+    if self.subcommands[subcommand].nil?
+      puts global.help
+      return
     else
-      self.subcommands[subcommands][:optParser].order!
+      if not self.subcommands[subcommand][:optParser].nil?
 
-      self.subcommands[subcommands][:action].call()
+        # Note: parse second level command if needed
+        self.subcommands[subcommand][:optParser].order!
+      end
+
+      self.subcommands[subcommand][:action].call(subcommandline)
     end
   end
 end
