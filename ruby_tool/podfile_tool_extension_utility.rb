a8bf9ec42833b3aec11445437d74e7694414cce5
daydreamboy <wesley4chen@gmail.com>
2022-10-11 17:33:34 +0800

    ruby_tool: add code

--- ruby_tool/podfile_tool_extension_utility.rb
+++ ruby_tool/podfile_tool_extension_utility.rb
@@ -7,9 +7,36 @@ module PodfileToolUtility
   end
 
   module ClassMethods
+    ##
+    # Get current xcode version
+    #
+    # @return [String] current xcode version
+    # 
     def xcode_version
       # @see https://groups.google.com/g/cocoapods/c/GFMujJUdVrY?pli=1
       `xcrun xcodebuild -version | head -1 | awk '{print $2}'`
+    end
+
+    ##
+    # check current xcode version > x.y.z
+    #
+    # @param [String] version_string, e.g. "14.0"
+    #
+    # @return [Boolean] true if xcode version > x.y.z, false if not
+    #
+    def xcode_version_greater_than(version_string)
+      Gem::Version.new(self.xcode_version) > Gem::Version.new(version_string)
+    end
+
+    ##
+    # check current xcode version >= x.y.z
+    #
+    # @param [String] version_string, e.g. "14.0"
+    #
+    # @return [Boolean] true if xcode version >= x.y.z, false if not
+    #
+    def xcode_version_greater_than_or_equal(version_string)
+      Gem::Version.new(self.xcode_version) >= Gem::Version.new(version_string)
     end
   end
 end
--- ruby_tool/tests/test_podfile_tool_extension_utility.rb
+++ ruby_tool/tests/test_podfile_tool_extension_utility.rb
@@ -3,3 +3,9 @@
 require_relative '../podfile_tool'
 
 puts PodfileTool.xcode_version
+
+puts PodfileTool.xcode_version_greater_than '14.0'
+puts PodfileTool.xcode_version_greater_than '13.0'
+puts PodfileTool.xcode_version_greater_than '13.1'
+puts PodfileTool.xcode_version_greater_than_or_equal '14.0'
+
