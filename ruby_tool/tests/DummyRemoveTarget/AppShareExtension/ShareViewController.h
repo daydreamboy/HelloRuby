1cf8a7b6e625c2c74f37c37052eaee3c004d120d
daydreamboy <wesley4chen@gmail.com>
2023-11-20 09:58:36 +0800

    ruby_tool: add test project

---
+++ ruby_tool/tests/DummyRemoveTarget/AppShareExtension/ShareViewController.h
@@ -0,0 +1,13 @@
+//
+//  ShareViewController.h
+//  AppShareExtension
+//
+//  Created by wesley_chen on 2023/11/17.
+//
+
+#import <UIKit/UIKit.h>
+#import <Social/Social.h>
+
+@interface ShareViewController : SLComposeServiceViewController
+
+@end
---
+++ ruby_tool/tests/DummyRemoveTarget/AppShareExtension/ShareViewController.m
@@ -0,0 +1,39 @@
+//
+//  ShareViewController.m
+//  AppShareExtension
+//
+//  Created by wesley_chen on 2023/11/17.
+//
+
+#import "ShareViewController.h"
+
+@interface ShareViewController ()
+
+@end
+
+@implementation ShareViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    self.placeholder = @"This is AppShareExtension";
+}
+
+- (BOOL)isContentValid {
+    // Do validation of contentText and/or NSExtensionContext attachments here
+    return YES;
+}
+
+- (void)didSelectPost {
+    // This is called after the user selects Post. Do the upload of contentText and/or NSExtensionContext attachments.
+    
+    // Inform the host that we're done, so it un-blocks its UI. Note: Alternatively you could call super's -didSelectPost, which will similarly complete the extension context.
+    [self.extensionContext completeRequestReturningItems:@[] completionHandler:nil];
+}
+
+- (NSArray *)configurationItems {
+    // To add configuration options via table cells at the bottom of the sheet, return an array of SLComposeSheetConfigurationItem here.
+    return @[];
+}
+
+@end
