fecf74b9101872c04cfb33c0b4ec318002fac8dd
daydreamboy <wesley4chen@gmail.com>
2023-11-20 10:00:23 +0800

    ruby_tool: add test project

--- ruby_tool/tests/DummyRemoveTarget/AnotherAppShareExtension/ShareViewController.m
+++ ruby_tool/tests/DummyRemoveTarget/AnotherAppShareExtension/ShareViewController.m
@@ -16,7 +16,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    self.placeholder = @"This is AppShareExtension";
+    self.placeholder = @"This is AnotherAppShareExtension";
 }
 
 - (BOOL)isContentValid {
